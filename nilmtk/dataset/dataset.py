b70896629076936d116ada72e0810795a503db22
Jack Kelly <jack-list@xlk.org.uk>
2013-12-04 17:52:24 +0000

    need new line at end of class docstring.

--- nilmtk/dataset/dataset.py
+++ nilmtk/dataset/dataset.py
@@ -2,7 +2,8 @@
 
 class DataSet(object):
     """Base class for all datasets.  This class can be used
-    for loading nilmtk's REDD+ data format."""
+    for loading nilmtk's REDD+ data format.
+    """
 
     # TODO: before we can implement this, we need to decide
     # how we're going to represent Buildings:
