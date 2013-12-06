a40064a51928b72cf296ed93b2168f303ae268a9
Jack Kelly <jack-list@xlk.org.uk>
2013-12-06 21:08:55 +0000

    Modified `Building` class as per discussions between Jack and Nipun earlier today on email.
    * Added `Electricity` class for holding electricity data and methods.
    
    * Replaced `map_appliance_to_upstream` and `map_circuit_to_mains` with a
      single `Electricity.wiring` attribute which is a NetworkX Directed Graph.
      Using NetworkX should reduce the amount code we have to write, whilst allowing
      us to represent a wiring system of arbitrary complexity (from a simple domestic
      house to a complex industrial wiring system).
    
    * The previous design didn't hold any information about the type of electricity
      meters used, or allow for circuits which may be instrumented with multiple
      meters.  Info about the class of each meter might be useful when calibrating
      and also during feature detection (some meters do very odd things to
      data!).  So I've made two tentative changes:
    
      1) `mains` and `circuits` column names now include a
         `meter_<K>` field so full column names will be of the form
         `mains_0_meter_1_apparent`.  This allows us to represent the
         situation where we might have multiple meters recording the
         same data (which is true in Jack's dataset where we use both
         our Sound Card Power Meter and a Current Cost CT clamp to
         measure a single mains feed)
    
      2) I added an `Electricity.meters` attribute to describe each class
         of meter and to specify which channels use that class of meter.
    
    * Added optional DataFrames in Building for:
      * weather
      * gas
      * water

--- nilmtk/building.py
+++ nilmtk/building.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from .electricity import Electricity
 
 class Building(object):
     """Represent a physical building (e.g. a domestic house).
@@ -15,117 +16,30 @@ class Building(object):
     rooms : list of strings, optional
         A list of room names. Use standard names for each room
 
-    electric: a collections.namedtuple (a little like a C struct) with fields:
+    electric : nilmtk Electricity object
 
-        mains : DataFrame, shape (n_samples, n_features), optional
-            The power measurements taken from the furthest upstream.
-            The index is a timezone-aware pd.DateTimeIndex
-            Use standard column names of the form `mains_<N>_<measurement>`:
-            * `N` is the phase or split.  Indexed from 0.
-            * `measurement` is one of `apparent` | `active` | `reactive` | `voltage`
-            For example: `mains_0_apparent`
+    gas : Series, optional
+        gas consumption in kWh
 
-        circuits : DataFrame, shape (n_samples, n_features), optional
-            The power measurements taken downstream of the mains measurements but
-            upstream of the appliances.
-            The index is a timezone-aware pd.DateTimeIndex
-            Use standard column names of the form `<circuit>_<N>_<measurement>`:
-            * `circuit` is the standard name for this circuit.
-            * `N` is the index for this circuit.  Indexed from 0.
-            * `measurement` is one of `apparent` | `active` | `reactive` | `voltage`
-            For example: `lighting_0_apparent`
+    water : Series, optional
+        water consumption in cubic meters
 
-        appliances : dict of DataFrames, optinal
-            Each key is an appliance name string in the form `<appliance>_<N>`:
-            * `appliance` is a standard appliance name.  For a list of valid
-               names, see nilmtk/docs/appliance_names.txt
-            * `N` is the index for that appliance within this building.
-               Indexed from 0.
-            Each value is a DataFrame shape (n_samples, n_features) where each
-            column name is one of `apparent` | `active` | `reactive` | `voltage`
-            and the index is a timezone-aware pd.DateTimeIndex
-
-        appliance_estimates : Panel (3D matrix), optional
-            Output from the NILM algorithm.
-            The index is a timezone-aware pd.DateTimeIndex
-            The first two dimensions are the same as for the appliance DataFrame
-            The third dimension describes, for each appliance and for each time:
-            * `power` : float. Estimated power consumption in Watts.
-            * `state` : int, optional.
-            * `confidence` : float [0,1], optional.
-            * `power_prob_dist` : object describing the probability dist, optional
-            * `state_prob_dist` : object describing the probability dist, optional
-
-        nominal_mains_voltage : float, optional
-
-        map_appliance_to_room : dict, optional
-            e.g. {`tv_0`    : `livingroom_0`,
-                  `fridge_0`: `kitchen_0`}
-
-        map_appliance_to_upstream : dict
-            Keys are appliance names of the form `<appliance>_<N>`
-            Values are *either* the circuit *or* the mains to which this
-            appliance is connected.
-
-        map_circuit_to_mains : dict
-            Each key is a circuit name of the form `<circuit>_<N>`
-            Each value is a mains name.
-
+    weather : DataFrame, optional
+        index is a timezone-aware DateTimeIndex
+        columns are names like `temperature` (degrees C) etc
 
     """
 
     def __init__(self):
-        geographic_coordinates = None
-        n_occupants = None
-        rooms = None
-        electric={}
-        '''
-        electric = namedtuple("electric", ["mains", "circuits", "appliances",
-                                           "appliance_estimates",
-                                           "nominal_mains_voltage",
-                                           "map_appliance_to_room",
-                                           "map_appliance_to_upstream",
-                                           "map_circuit_to_mains"])
-
-        '''
-    def set_mains(self, mains):
-        self.electric.mains = mains
-
-    def get_electric(self):
-        return self.electric
-
-    def get_appliance(self, appliance_name, measurement="all"):
-        """
-        Arguments
-        ---------
-        appliance_name : string
-        measurement : string or list of strings, optional
-            apparent | active | reactive | voltage | all
-
-        Returns
-        -------
-        appliance_data : DataFrame
-        """
-        raise NotImplementedError
-
-    def count_appliances(self, appliance_name):
-        """
-        Returns
-        -------
-        n_appliances : int
-        """
-        raise NotImplementedError
-
-    def get_vampire_power(self):
-        raise NotImplementedError
-
-    def get_diff_between_aggregate_and_appliances(self):
-        raise NotImplementedError
+        self.geographic_coordinates = None
+        self.n_occupants = None
+        self.rooms = []
+        self.electric = Electricity()
+        self.gas = None
+        self.water = None
+        self.weather = None
 
     def crop(self, start, end):
         """Reduce all timeseries to just these dates"""
         raise NotImplementedError
 
-    def plot_appliance_activity(self, source):
-        """Plot a compact representation of all appliance activity."""
-        raise NotImplementedError
---
+++ nilmtk/electricity.py
@@ -0,0 +1,123 @@
+class Electricity(object):
+    """Store and process electricity for a building.
+
+    Attributes
+    ----------
+
+    mains : DataFrame, shape (n_samples, n_features), optional
+        The power measurements taken from the level furthest upstream.
+        The index is a timezone-aware pd.DateTimeIndex
+        Use standard column names of the form `mains_<N>_meter_<K>_<measurement>`:
+        * `N` is the phase or split.  Indexed from 0.
+        * `K` is the numeric ID of the meter. Indexed from 0.
+           Indexes into the `meters` dict.
+        * `measurement` is one of `apparent` | `active` | `reactive` | `voltage`
+        For example, if we had a dataset recorded in the UK where the home has
+        only a single phase supply but uses two separate meters, the first of
+        which measures active and reactive; the second of which measures only
+        active, then we'd use:
+            * `mains_0_meter_0_active`
+            * `mains_0_meter_0_reactive`
+            * `mains_0_meter_1_active`
+
+    circuits : DataFrame, shape (n_samples, n_features), optional
+        The power measurements taken downstream of the mains measurements but
+        upstream of the appliances.
+        The index is a timezone-aware pd.DateTimeIndex
+        Use standard column names of the form `<circuit>_<N>_meter_<K>_<measurement>`:
+        * `circuit` is the standard name for this circuit.
+        * `N` is the index for this circuit.  Indexed from 0.
+        * `K` is the numeric ID of the meter. Indexed from 0.
+          Indexes into the `meters` dict.
+        * `measurement` is one of `apparent` | `active` | `reactive` | `voltage`
+        For example: `lighting_0_apparent`
+
+    appliances : dict of DataFrames, optinal
+        Each key is an appliance name string in the form `<appliance>_<N>`:
+        * `appliance` is a standard appliance name.  For a list of valid
+           names, see nilmtk/docs/appliance_names.txt
+        * `N` is the index for that appliance within this building.
+           Indexed from 0.
+        Each value is a DataFrame shape (n_samples, n_features) where each
+        column name is one of `apparent` | `active` | `reactive` | `voltage`
+        and the index is a timezone-aware pd.DateTimeIndex
+
+    appliance_estimates : Panel (3D matrix), optional
+        Output from the NILM algorithm.
+        The index is a timezone-aware pd.DateTimeIndex
+        The first two dimensions are the same as for the appliance DataFrame
+        The third dimension describes, for each appliance and for each time:
+        * `power` : float. Estimated power consumption in Watts.
+        * `state` : int, optional.
+        * `confidence` : float [0,1], optional.
+        * `power_prob_dist` : object describing the probability dist, optional
+        * `state_prob_dist` : object describing the probability dist, optional
+
+    nominal_mains_voltage : float, optional
+
+    map_appliance_to_room : dict, optional
+        e.g. {`tv_0`    : `livingroom_0`,
+              `fridge_0`: `kitchen_0`}
+
+    wiring : networkx.DiGraph
+        Nodes appliance names or circuit names or mains names.
+        Edges describe the wiring between mains, circuits and appliances.
+        Edge direction indicates the flow of energy.  i.e. edges point
+        towards loads.
+
+    meters : list of dicts, optional
+        Each element describes a class of meter used to record mains or
+        circuit data.  For example:
+
+        [{'manufacturer': 'Current Cost', 
+          'model': 'EnviR', 
+          'type': 'CT',
+          'accuracy_class': 'C',
+          'used_by': [0, 1, 'fridge0', 'kettle0']}]
+
+        `used_by` maps to a list of all the channels which use this
+        class of meter.  This list may contain numeric meter IDs (the `K` 
+        parameter in mains and circuit labels) and/or appliance names.
+    """
+
+    def __init__(self):
+        self.mains = None
+        self.circuits = None
+        self.appliances = None
+        self.appliance_estimates = None
+        self.nominal_mains_voltage = None
+        self.map_appliance_to_room = {}
+        self.wiring = None
+        self.meters = []
+
+    def get_appliance(self, appliance_name, measurement="all"):
+        """
+        Arguments
+        ---------
+        appliance_name : string
+        measurement : string or list of strings, optional
+            apparent | active | reactive | voltage | all
+
+        Returns
+        -------
+        appliance_data : DataFrame
+        """
+        raise NotImplementedError
+
+    def count_appliances(self, appliance_name):
+        """
+        Returns
+        -------
+        n_appliances : int
+        """
+        raise NotImplementedError
+
+    def get_vampire_power(self):
+        raise NotImplementedError
+
+    def get_diff_between_aggregate_and_appliances(self):
+        raise NotImplementedError
+
+    def plot_appliance_activity(self, source):
+        """Plot a compact representation of all appliance activity."""
+        raise NotImplementedError
