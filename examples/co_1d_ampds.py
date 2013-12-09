6654461e804e619169179be8a00a71862a4901a8
nipunreddevil <nipunreddevil@gmail.com>
2013-12-09 15:39:01 +0530

    Added AMPds model; CO WORKS!

--- examples/co_1d_ampds.py
+++ examples/co_1d_ampds.py
@@ -1,9 +1,15 @@
-from __future__ import print_function
 from nilmtk.dataset import ampds
+from nilmtk.disaggregate.co_1d import CO_1d
+import json
+import pandas as pd
 
 dataset = ampds.AMPDS()
 
 PATH = '/home/nipun/Desktop/AMPds/'
+
+# Feature to perform disaggregation on
+DISAGG_FEATURE = 'power_active'
+
 
 # Load data
 dataset.load(PATH)
@@ -11,37 +17,58 @@ dataset.load(PATH)
 # Get data of Home_01
 building = dataset.buildings['Home_01']
 
+print('Loading data....')
 # Get mains data
 mains = building.utility.electric.mains
+
+# Get appliances data
+app = building.utility.electric.appliances
 
 # Finding number of data points
 row = mains.index.size
 
-# Load model
+print('Dividing data into test and train')
+# Dividing the data into train and test (ratio 1:9)
+train_aggregate, test_aggregate = mains[:row / 10], mains[row / 10:]
+
+# Finding the active component of power for train and test aggregate (mains)
+train_power_aggregate = train_aggregate[DISAGG_FEATURE]
+test_power_aggregate = test_aggregate[DISAGG_FEATURE]
+
+# Loading train and test appliance data
+train_appliance = {}
+test_appliance = {}
+for appliance in app:
+    train_appliance[appliance] = app[appliance][:row / 10]
+    test_appliance[appliance] = app[appliance][row / 10:]
+
+# Finding the active component of power for appliances
+train_power_appliance = {}
+test_power_appliance = {}
+for appliance in app:
+    train_power_appliance[appliance] = train_appliance[appliance][DISAGG_FEATURE]
+    test_power_appliance[appliance] = test_appliance[appliance][DISAGG_FEATURE]
+
+# Creating appliances dataframe
+df_train_appliances = pd.DataFrame(train_power_appliance)
+
+# Instantiating the CO Disaggregator
+disaggregator = CO_1d()
+
+# Train
+
+# Load learnt model from disk
+with open('ampds_model_co.json') as f:
+    model = json.load(f)
+
+print(model)
+disaggregator.model = model
+print("*"*80)
+
+print('Disaggregate!')
+# Predicting with the learnt model
+disaggregator.disaggregate(test_power_aggregate)
 
 
-# Get appliances data
 
-
-# Name of buildings
-pecan_1min.load_building_names('/home/nipun/Desktop/PECAN/')
-
-# Loading data for Home 01
-pecan_1min.load_building('/home/nipun/Desktop/PECAN/', 'Home 01')
-
-# Accessing Home 01 Building
-building = pecan_1min.buildings['Home_01']
-
-# Summary of Mains
-print("Mains summary")
-print(building.utility.electric.mains.describe())
-
-# Summary of appliances
-print("Appliances summary")
-for appliance in building.utility.electric.appliances:
-    print(building.utility.electric.appliances[appliance].describe())
-
-# Loading all buildings data
-pecan_1min_complete = pecan.Pecan_1min()
-pecan_1min_complete.load('/home/nipun/Desktop/PECAN/')
 
