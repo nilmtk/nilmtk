658cb57c12a5facb75808644c2ea55d4aef28564
Jack Kelly <jack-list@xlk.org.uk>
2013-12-11 11:08:07 +0000

    Tweaking definition of Electricity class
    * use namedtuples for DataFrame columns to make it easier to parse
    * represent multiple appliances measured on a single `appliance`
      channel
    * rename `map_appliance_to_room` to `appliances_in_each_room`
    * `appliances_in_each_room` maps from rooms to a list of appliances
    * `meters` now maps from a standard (<meter manufacturer>, <model>)
      tuple to a list of all the channels which use that meter.  The
      intention is that we'll have a `meters.json` file somewhere which
      lists all the meter makes and models that we care about, along with
      some useful metadata.
    * The doc string for `appliance_metadata` has been updated to reflect
      our discussions on issue #40

--- nilmtk/sensors/electricity.py
+++ nilmtk/sensors/electricity.py
@@ -7,48 +7,54 @@ class Electricity(object):
     mains : DataFrame, shape (n_samples, n_features), optional
         The power measurements taken from the level furthest upstream.
         The index is a timezone-aware pd.DateTimeIndex
-        Use standard column names of the form
-        `mains_<N>_meter_<K>_<measurement>` where:
+        Each column name is a MainsName namedtuple with fields:
 
-        * `N` is the phase or split.  Indexed from 1.
-        * `K` is the numeric ID of the meter. Indexed from 1.
-           Indexes into the `meters` dict.
+        * `split` is the phase or split.  Indexed from 1.
+        * `meter` is the numeric ID of the meter. Indexed from 1.
         * `measurement` is one of `apparent` | `active` | `reactive` | `voltage`
         For example, if we had a dataset recorded in the UK where the home has
         only a single phase supply but uses two separate meters, the first of
         which measures active and reactive; the second of which measures only
         active, then we'd use:
-            * `mains_1_meter_1_active`
-            * `mains_1_meter_1_reactive`
-            * `mains_1_meter_2_active`
+            * `MainsName(split=1, meter=1, measurement='active')`
+            * `MainsName(split=1, meter=1, measurement='reactive')`
+            * `MainsName(split=1, meter=2, measurement='active')`
 
     circuits : DataFrame, shape (n_samples, n_features), optional
         The power measurements taken downstream of the mains measurements but
         upstream of the appliances.
         The index is a timezone-aware pd.DateTimeIndex
-        Use standard column names of the form `<circuit>_<N>_meter_<K>
-        _<measurement>`:
+        Each column name is a CircuitName namedtuple with fields:
         * `circuit` is the standard name for this circuit.
-        * `N` is the index for this circuit.  Indexed from 1.
-        * `K` is the numeric ID of the meter. Indexed from 1.
+        * `split` is the index for this circuit.  Indexed from 1.
+        * `meter` is the numeric ID of the meter. Indexed from 1.
           Indexes into the `meters` dict.
         * `measurement` is one of `apparent` | `active` | `reactive` | `voltage`
-        For example: `lighting_1_apparent`
+
+        For example: 
+        `CircuitName(circuit='lighting', split=1, 
+                     meter=3, measurement='apparent')`
 
     appliances : dict of DataFrames, optional
-        Each key is an appliance name string in the form `<appliance>_<N>`:
-        * `appliance` is a standard appliance name.  For a list of valid
+        Each key is an ApplianceName namedtuple with fields:
+        * `name` is a standard appliance name.  For a list of valid
            names, see nilmtk/docs/standard_names/appliances.txt
-        * `N` is the index for that appliance within this building.
+        * `instance` is the index for that appliance within this building.
            Indexed from 1.
+        * `measurement` is one of 'apparent' | 'active' | 'reactive' | 'voltage'
+        For example, if a house has two TVs then use these two column names:
+        `('tv', 1, 'active'), ('tv', 2, 'active')`
         Each value is a DataFrame shape (n_samples, n_features) where each
         column name is one of `apparent` | `active` | `reactive` | `voltage`
-        and the index is a timezone-aware pd.DateTimeIndex
+        and the index is a timezone-aware pd.DateTimeIndex.
+        If multiple appliances are monitored on one channel (e.g. tv + dvd)
+        then use a tuple of appliances as the column name, e.g.:
+        `(('tv', 1, 'active'), ('dvd player', 1, 'active'))`
 
     appliance_estimates : Panel (3D matrix), optional
         Output from the NILM algorithm.
         The index is a timezone-aware pd.DateTimeIndex
-        The first two dimensions are the same as for the appliance DataFrame
+        The first two dimensions are the same as for the `appliances` DataFrame
         The third dimension describes, for each appliance and for each time:
         * `power` : float. Estimated power consumption in Watts.
         * `state` : int, optional.
@@ -58,33 +64,42 @@ class Electricity(object):
 
     nominal_mains_voltage : float, optional
 
-    map_appliance_to_room : dict, optional
-        e.g. {`tv_1`    : `livingroom_1`,
-              `fridge_1`: `kitchen_1`}
+    appliances_in_each_room : dict, optional
+        Each key is a (<room name>, <room instance>) tuple
+        (as used in this `Building.rooms`).
+        Each value is a list of (<appliance name>, <instance>) tuples
+        e.g. `{('livingroom', 1): [('tv', 1), ('dvd', 2)]}`
 
     wiring : networkx.DiGraph
-        Nodes appliance names or circuit names or mains names.
+        Nodes are ApplianceNames or CircuitNames or MainsNames.
         Edges describe the wiring between mains, circuits and appliances.
         Edge direction indicates the flow of energy.  i.e. edges point
         towards loads.
 
-    meters : list of dicts, optional
-        Each element describes a class of meter used to record mains or
-        circuit data.  For example:
-
-        [{'manufacturer': 'Current Cost',
-          'model': 'EnviR',
-          'type': 'CT',
-          'accuracy_class': 'C',
-          'used_by': [1, 2, 'fridge1', 'kettle1']}]
-
-        `used_by` maps to a list of all the channels which use this
-        class of meter.  This list may contain numeric meter IDs (the `K`
-        parameter in mains and circuit labels) and/or appliance names.
+    meters : dict, optional
+        Maps from a tuple (<meter manufacturer>, <model>) to a list of 
+        all the channels which use that type of meter.  Types of meters
+        are described in `docs/standard_names/meters.json`.  e.g.:
+        `{
+           ('Current Cost', 'EnviR') : 
+             [
+                MainsName(split=1, meter=1, measurement='apparent'),
+                ApplianceName(name='boiler', instance=1, measurement='apparent')
+             ]
+          }`
 
     appliance_metadata : dict of dicts, optional
-        Metadata describing each appliance.  e.g.
-        {'television_1': {'type': 'CRT', 'year of manufacture': 2001}}
+        Metadata describing each appliance.  The permitted fields and values
+        for each appliance name are described in `appliances.json`.  e.g.
+
+        `{ApplianceName(name='tv', instance=1): 
+            {'display': 'lcd', 
+             'backlight': 'led'
+             'screen size in inches': 42,
+             'year of manufacture': 2001,
+             'last time seen active': '3/4/2012'
+            }
+        }`
     """
 
     def __init__(self):
@@ -93,9 +108,10 @@ class Electricity(object):
         self.appliances = None
         self.appliance_estimates = None
         self.nominal_mains_voltage = None
-        self.map_appliance_to_room = {}
+        self.appliances_in_each = {}
         self.wiring = None
-        self.meters = []
+        self.meters = {}
+        self.appliance_metadata = {}
 
     def get_appliance(self, appliance_name, measurement="all"):
         """
