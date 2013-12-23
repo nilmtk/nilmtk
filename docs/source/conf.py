068abc24ecf61ad2be107bf0f5e3f3b0584efa69
nipunreddevil <nipunreddevil@gmail.com>
2013-12-23 19:14:44 +0530

    Added more paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,7 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../nilmtk/'))
+sys.path.insert(0, os.path.abspath('../../nilmtk/nilmtk/'))
 sys.path.insert(0, os.path.abspath('../../nilmtk/nilmtk'))
 sys.path.insert(0, os.path.abspath('../../nilmtk/nilmtk/sensors/'))
 sys.path.insert(0, '/usr/local/lib/python2.7/dist-packages/')
@@ -31,18 +32,14 @@ sys.path.insert(0, os.path.abspath('sphinxext'))
 print sys.path
 
 
-
-
-
-# -- General configuration -----------------------------------------------------
-
+# -- General configuration -----------------------------------------------
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
-
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode',
-			'sphinx.ext.intersphinx', 'numpydoc', 'inheritance_diagram', 'ipython_console_highlighting']
+extensions = [
+    'sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx', 'numpydoc', 'inheritance_diagram', 'ipython_console_highlighting']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['ntemplates']
@@ -104,7 +101,7 @@ pygments_style = 'sphinx'
 #modindex_common_prefix = []
 
 
-# -- Options for HTML output ---------------------------------------------------
+# -- Options for HTML output ---------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
@@ -186,24 +183,24 @@ html_last_updated_fmt = '%b %d, %Y'
 htmlhelp_basename = 'nilmtkdoc'
 
 
-# -- Options for LaTeX output --------------------------------------------------
+# -- Options for LaTeX output --------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'nilmtk.tex', u'nilmtk Documentation',
-   u'nilmtk authors', 'manual'),
+    ('index', 'nilmtk.tex', u'nilmtk Documentation',
+     u'nilmtk authors', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -227,7 +224,7 @@ latex_documents = [
 #latex_domain_indices = True
 
 
-# -- Options for manual page output --------------------------------------------
+# -- Options for manual page output --------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
@@ -240,15 +237,15 @@ man_pages = [
 #man_show_urls = False
 
 
-# -- Options for Texinfo output ------------------------------------------------
+# -- Options for Texinfo output ------------------------------------------
 
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'nilmtk', u'nilmtk Documentation',
-   u'nilmtk authors', 'nilmtk', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'nilmtk', u'nilmtk Documentation',
+     u'nilmtk authors', 'nilmtk', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
