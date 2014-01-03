ac626c258b3d53dbb828a1ce1bae524b890fc721
Jack Kelly <jack-list@xlk.org.uk>
2014-01-03 21:21:30 +0000

    `insert_zeros` is finished (except for unittests)

--- nilmtk/preprocessing/electricity/single.py
+++ nilmtk/preprocessing/electricity/single.py
@@ -4,18 +4,20 @@ from __future__ import print_function, division
 import numpy as np
 import pandas as pd
 from copy import deepcopy
+from nilmtk.stats.electricity.single import get_sample_period
 
 def insert_zeros(single_appliance_dataframe, max_sample_period=None):
-    """Some individual appliance monitors (IAM) get turned off occasionally.
+    """Some individual appliance monitors (IAMs) get turned off.
     This might happen, for example, in the case where a hoover's IAM is 
     permanently attached to the hoover's power cord, even when the hoover is
-    put away in the cupboard.
+    unplugged and put away in the cupboard.
 
     Say the hoover was switched on and then both the hoover and the hoover's IAM
     were unplugged.  This would result in the dataset having a gap immediately
-    after the on-segment.  This combination of an on-segment followed (without
+    after an on-segment.  This combination of an on-segment followed (without
     any zeros) by a gap might confuse downstream statistics and
-    disaggregation functions.
+    disaggregation functions which assume that the power drawn by an appliance
+    between readings i and j is held constant at i watts.
 
     If, after any reading > 0, there is a gap in the dataset of more than 
     `max_sample_period` seconds then assume the appliance (and 
@@ -30,41 +32,48 @@ def insert_zeros(single_appliance_dataframe, max_sample_period=None):
     Parameters
     ----------
     single_appliance_dataframe : pandas.DataFrame
-        A DataFrame storing data from a single appliance
+        Data from a single appliance.
 
     max_sample_period : float or int, optional
         The maximum sample permissible period (in seconds). Any gap longer
-        than `max_sample_period` is assumed to mean that the IAM 
+        than `max_sample_period` is assumed to imply that the IAM 
         and appliance are off.  If None then will default to
-        3 x the sample period of `single_appliance_dataframe`.
+        4 x the sample period of `single_appliance_dataframe`.
 
     Returns
     -------
     df_with_zeros : pandas.DataFrame
         A copy of `single_appliance_dataframe` with zeros inserted 
-        `max_sample_period` seconds after the last sample of the on-segment.
+        `max_sample_period` seconds after the last sample of each on-segment.
     
     """
-    # TODO: handle DataFrame
+    if max_sample_period is None:
+        max_sample_period = get_sample_period(single_appliance_dataframe) * 4
 
+    # Make a copy (the copied dataframe is what we return, after inserting zeros)
     df_with_zeros = deepcopy(single_appliance_dataframe)
 
+    # Get the length of time between each pair of consecutive samples. Seconds.
     timedeltas = np.diff(df_with_zeros.index.values) / np.timedelta64(1, 's')
     readings_before_gaps = df_with_zeros[:-1][timedeltas > max_sample_period]
-    # we only add a 0 if the preceeding value is > 0
-    readings_before_gaps = readings_before_gaps[readings_before_gaps > 0]
-    
-    zeros = pd.Series(0,
-                      index=readings_before_gaps.index +
-                      pd.DateOffset(seconds=max_sample_period))
+
+    # we only add a 0 if the recorded value just before the gap is > 0
+    readings_before_gaps = readings_before_gaps[readings_before_gaps.sum(axis=1) > 0]
+
+    # Make a DataFrame of zeros, ready for insertion
+    dates_to_insert_zeros = (readings_before_gaps.index + 
+                             pd.DateOffset(seconds=max_sample_period))
+    zeros = pd.DataFrame(data=0, index=dates_to_insert_zeros, 
+                         columns=df_with_zeros.columns, dtype=np.float32)
+
+    # Insert the dataframe of zeros into the data.
     df_with_zeros = df_with_zeros.append(zeros)
     df_with_zeros = df_with_zeros.sort_index()
-
     return df_with_zeros
 
 
 def replace_nans_with_zeros(multiple_appliances_dataframe, max_sample_period):
     """For a single column, find gaps of > max_sample_period and replace
-    all NaNs in these gaps with zeros.  But leave NaNs is gaps <=
-    max_sample_period."""
+    all NaNs in these gaps with zeros.  
+    But leave NaNs is gaps <= max_sample_period."""
     raise NotImplementedError
