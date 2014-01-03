5b673df765a39c7f94d5be04e3421f5d93795f8b
Jack Kelly <jack-list@xlk.org.uk>
2014-01-03 20:26:20 +0000

    Can now pass **kwargs to `ax.plot()`

--- nilmtk/plots.py
+++ nilmtk/plots.py
@@ -5,7 +5,7 @@ import numpy as np
 
 _to_ordinalf_np_vectorized = np.vectorize(mdates._to_ordinalf)
 
-def plot_series(series, ax=None, label=None, date_format='%d/%m/%y %H:%M:%S', **kwargs):
+def plot_series(series, **kwargs):
     """Plot function for series which is about 5 times faster than
     pd.Series.plot().
 
@@ -14,19 +14,19 @@ def plot_series(series, ax=None, label=None, date_format='%d/%m/%y %H:%M:%S', **
     ax : matplotlib Axes, optional
         If not provided then will generate our own axes.
 
-    label : str, optional
-        The label for the plotted line. The
-        caller is responsible for enabling the legend.
-
     date_format : str, optional, default='%d/%m/%y %H:%M:%S'
+
+    Can also use all **kwargs expected by `ax.plot`
     """
+    ax = kwargs.get('ax')
+    date_format = kwargs.get('date_format', '%d/%m/%y %H:%M:%S')
+
     if ax is None:
         fig, ax = plt.subplots(1)
     x = _to_ordinalf_np_vectorized(series.index.to_pydatetime())
-    ax.plot(x, series, label=label, **kwargs)
+    ax.plot(x, series, **kwargs)
     ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format, 
                                                       tz=series.index.tzinfo))
     ax.set_ylabel('watts')
     fig.autofmt_xdate()
-    plt.draw()
     return ax
