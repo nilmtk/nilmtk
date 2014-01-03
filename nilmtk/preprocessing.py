b64690e329446a0aea179f33e36765c3f909d9f8
nipunreddevil <nipunreddevil@gmail.com>
2014-01-03 11:25:08 +0530

    Added downsample function in preprocessing; closes #71

--- nilmtk/preprocessing.py
+++ nilmtk/preprocessing.py
@@ -1,4 +1,5 @@
 """ Contains preprocessing modules"""
+import pandas as pd
 
 from nilmtk.stats.electricity.building import top_k_appliances
 from copy import deepcopy
@@ -10,4 +11,35 @@ def filter_top_k_appliances(building, k=5):
     appliances_dict = building.utility.electric.appliances
     appliances_filtered = {appliance: appliances_dict[appliance] for appliance in appliances_dict if appliance in top_k}
     building_copy.utility.electric.appliances = appliances_filtered
+    return building_copy
+
+
+def downsample(building, rule='1T', how='mean'):
+    """Downsample all electrical data
+
+    Parameters
+    ----------
+    building : nilmtk.Building
+    rule : string
+        refer to pandas.resample docs for rules; default '1T' or 1 minute
+    how : string
+        refer to pandas.resample docs for how; default 'mean'
+
+    Returns
+    --------
+    building_copy: nilmtk.Building
+
+    """
+    building_copy = deepcopy(building)
+
+    # Downsampling appliance data
+    for appliance_name, appliance_df in building.utility.electric.appliances.iteritems():
+        building_copy.utility.electric.appliances[
+            appliance_name] = appliance_df.resample(rule, how)
+
+    # Downsampling mains data
+    for mains_name, mains_df in building.utility.electric.mains.iteritems():
+        building_copy.utility.electric.mains[
+            mains_name] = mains_df.resample(rule, how)
+
     return building_copy
