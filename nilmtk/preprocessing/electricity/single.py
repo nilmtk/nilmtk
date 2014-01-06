c91fe511c5dc52bf5a0779188c94ee32770d3cde
Jack Kelly <jack-list@xlk.org.uk>
2014-01-06 08:55:41 +0000

    Updating docstring for `insert_zeros` to make it explicit that there are two possible reasons for lost samples: 1) a broken IAM 2) the IAM has been unplugged.   I think it's only the user who can decide which of these two assumptions best fits their data.  insert_zeros is applicable only in case 2.

--- nilmtk/preprocessing/electricity/single.py
+++ nilmtk/preprocessing/electricity/single.py
@@ -10,7 +10,17 @@ from nilmtk.sensors.electricity import Measurement
 
 
 def insert_zeros(single_appliance_dataframe, max_sample_period=None):
-    """Some individual appliance monitors (IAMs) get turned off.
+    """There are two possible reasons for lost samples in individual
+    appliance data: 
+
+    1) a broken IAM (hence we do not have any information about the appliance)
+    2) the IAM and appliance have been unplugged (hence we can infer that the
+       appliance is off)
+
+    Only the user who can decide which of these two assumptions best
+    fits their data.  insert_zeros is applicable only in case 2.
+
+    Some individual appliance monitors (IAMs) get turned off.
     This might happen, for example, in the case where a hoover's IAM is 
     permanently attached to the hoover's power cord, even when the hoover is
     unplugged and put away in the cupboard.
@@ -20,7 +30,7 @@ def insert_zeros(single_appliance_dataframe, max_sample_period=None):
     after an on-segment.  This combination of an on-segment followed (without
     any zeros) by a gap might confuse downstream statistics and
     disaggregation functions which assume that the power drawn by an appliance
-    between readings i and j is held constant at i watts.
+    between reading[i] and reading[i+1] is held constant at reading[i] watts.
 
     If, after any reading > 0, there is a gap in the dataset of more than 
     `max_sample_period` seconds then assume the appliance (and 
@@ -48,7 +58,7 @@ def insert_zeros(single_appliance_dataframe, max_sample_period=None):
     df_with_zeros : pandas.DataFrame
         A copy of `single_appliance_dataframe` with zeros inserted 
         `max_sample_period` seconds after the last sample of each on-segment.
-    
+
     """
     if max_sample_period is None:
         max_sample_period = get_sample_period(single_appliance_dataframe) * 4
