3051529cfbdbe344a13cb465162b5b512e7c632d
Jack Kelly <jack-list@xlk.org.uk>
2014-01-07 17:01:13 +0000

    see if this allows readthedocs to import code again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,8 +83,8 @@ def all_from(folder='', abspath=None):
             print "adding", i
             print "/n"
 
-# all_from('../../../nilmtk/')
-# all_from('../../nilmtk/')
+all_from('../../../nilmtk/')
+all_from('../../nilmtk/')
 
 #from nilmtk.mock import Mock
 
