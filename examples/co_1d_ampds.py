21791a8ccdb8b2e46d5fd443d1a92f233015d91b
nipunreddevil <nipunreddevil@gmail.com>
2014-06-16 15:53:54 +0530

    Corrected co_1d_ampds

--- examples/co_1d_ampds.py
+++ examples/co_1d_ampds.py
@@ -4,78 +4,54 @@ import json
 import pandas as pd
 from pandas import HDFStore
 from nilmtk.dataset.ampds import Measurement
+import nilmtk.preprocessing.electricity.building as prepb
+from nilmtk.cross_validation import train_test_split
 
 
 dataset = ampds.AMPDS()
 
-PATH = '/home/nipun/Desktop/AMPds/'
+PATH = '/home/nipun/study/datasets/AMPds/'
 
 # Feature to perform disaggregation on
 DISAGG_FEATURE = Measurement('power', 'active')
-
 
 # Load data
 dataset.load(PATH)
 
 # Get data of Home_01
-building = dataset.buildings['Building_1']
+building = dataset.buildings[1]
 
+# Let us filter out appliances contributing less than 5%
+building = prepb.filter_contribution_less_than_x(building, x=5)
 
-print('Loading data....')
-# Get mains data
-mains = building.utility.electric.mains[
-    building.utility.electric.mains.keys()[0]]
-
-# Get appliances data
-app = building.utility.electric.appliances
-
-# Finding number of data points
-row = mains.index.size
 
 print('Dividing data into test and train')
-# Dividing the data into train and test (ratio 1:9)
-train_aggregate, test_aggregate = mains[:row / 10], mains[row / 10: row / 5]
-
-# Finding the active component of power for train and test aggregate (mains)
-train_power_aggregate = train_aggregate[DISAGG_FEATURE]
-test_power_aggregate = test_aggregate[DISAGG_FEATURE]
-
-# Loading train and test appliance data
-train_appliance = {}
-test_appliance = {}
-for appliance in app:
-    train_appliance[appliance] = app[appliance][:row / 10]
-    test_appliance[appliance] = app[appliance][row / 10:row/5]
-
-# Finding the active component of power for appliances
-train_power_appliance = {}
-test_power_appliance = {}
-for appliance in app:
-    train_power_appliance[appliance] = train_appliance[
-        appliance][DISAGG_FEATURE]
-    test_power_appliance[appliance] = test_appliance[appliance][DISAGG_FEATURE]
-
-# Creating appliances dataframe
-df_train_appliances = pd.DataFrame(train_power_appliance)
-
-# Instantiating the CO Disaggregator
-disaggregator = CO_1d()
+train, test = train_test_split(building, train_size = 0.5) 
 
 # Train
-disaggregator.train(train_power_aggregate, train_power_appliance)
+disaggregator = CO_1d()
+disaggregator_name = "CO_1d"
+t1 = time.time()
+disaggregator.train(train, disagg_features=[DISAGG_FEATURE])
+t2 = time.time()
+print("Runtime to train for {} = {:.2f} seconds".format(disaggregator_name, t2 - t1))
+train_time=t2-t1
+    
+# Disaggregate
+t1 = time.time()
+disaggregator.disaggregate(test)
+t2 = time.time()
+print("Runtime to disaggregate for {}= {:.2f} seconds".format(disaggregator_name, t2 - t1))
+disaggregate_time=t2-t1   
+        
+# Predicted power and states
+# Predicted power is a DataFrame containing the predicted power of different 
+# appliances
+predicted_power = disaggregator.predictions
+app_ground = test.utility.electric.appliances
+ground_truth_power = pd.DataFrame({appliance: app_ground[appliance][DISAGG_FEATURE] for appliance in app_ground})
+        
 
-'''
-# Load learnt model from disk
-with open('ampds_model_co.json') as f:
-    model = json.load(f)
-'''
 
-'''
-print(model)
-disaggregator.model = model
-print("*" * 80)
-'''
 
-print('Disaggregate!')
-# Predicting with the learnt model
-disaggregator.disaggregate(test_power_aggregate)
+
