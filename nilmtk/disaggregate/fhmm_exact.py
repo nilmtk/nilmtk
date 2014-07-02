3407be666b4513557745043a26e98294a83e936f
Jack Kelly <jack-list@xlk.org.uk>
2014-07-02 22:55:26 +0100

    adding fhmm_exact and metrics from master branch

---
+++ nilmtk/disaggregate/fhmm_exact.py
@@ -0,0 +1,291 @@
+from nilmtk.utils import find_nearest
+from nilmtk.utils import find_nearest_vectorized
+from nilmtk.disaggregate.disaggregator import Disaggregator
+from nilmtk.sensors.electricity import Measurement
+from nilmtk.preprocessing.electricity.single import contiguous_blocks
+from nilmtk.stats.electricity.single import get_sample_period
+
+
+import pandas as pd
+import itertools
+import numpy as np
+from sklearn import metrics
+from sklearn import hmm
+
+from copy import deepcopy
+from collections import OrderedDict
+
+SEED = 42
+
+# Fix the seed for repeatibility of experiments
+np.random.seed(SEED)
+
+
+def sort_startprob(mapping, startprob):
+    """ Sort the startprob according to power means; as returned by mapping
+    """
+    num_elements = len(startprob)
+    new_startprob = np.zeros(num_elements)
+    for i in xrange(len(startprob)):
+        new_startprob[i] = startprob[mapping[i]]
+    return new_startprob
+
+
+def sort_covars(mapping, covars):
+    num_elements = len(covars)
+    new_covars = np.zeros_like(covars)
+    for i in xrange(len(covars)):
+        new_covars[i] = covars[mapping[i]]
+    return new_covars
+
+
+def sort_transition_matrix(mapping, A):
+    """ Sorts the transition matrix according to increasing order of 
+    power means; as returned by mapping
+
+    Parameters
+    ----------
+    mapping : 
+    A : numpy.array of shape (k, k)
+        transition matrix
+    """
+    num_elements = len(A)
+    A_new = np.zeros((num_elements, num_elements))
+    for i in range(num_elements):
+        for j in range(num_elements):
+            A_new[i, j] = A[mapping[i], mapping[j]]
+    return A_new
+
+
+def sort_learnt_parameters(startprob, means, covars, transmat):
+    mapping = return_sorting_mapping(means)
+    means_new = np.sort(means, axis=0)
+    startprob_new = sort_startprob(mapping, startprob)
+    covars_new = sort_covars(mapping, covars)
+    transmat_new = sort_transition_matrix(mapping, transmat)
+    assert np.shape(means_new) == np.shape(means)
+    assert np.shape(startprob_new) == np.shape(startprob)
+    assert np.shape(transmat_new) == np.shape(transmat)
+
+    return [startprob_new, means_new, covars_new, transmat_new]
+
+
+def compute_A_fhmm(list_A):
+    """
+    Parameters
+    -----------
+    list_pi : List of PI's of individual learnt HMMs
+
+    Returns
+    --------
+    result : Combined Pi for the FHMM
+    """
+    result = list_A[0]
+    for i in range(len(list_A) - 1):
+        result = np.kron(result, list_A[i + 1])
+    return result
+
+
+def compute_means_fhmm(list_means):
+    """
+    Returns 
+    -------
+    [mu, cov]
+    """
+
+    #list_of_appliances_centroids=[ [appliance[i][0] for i in range(len(appliance))] for appliance in list_B]
+    states_combination = list(itertools.product(*list_means))
+    num_combinations = len(states_combination)
+    means_stacked = np.array([sum(x) for x in states_combination])
+    means = np.reshape(means_stacked, (num_combinations, 1))
+    cov = np.tile(5 * np.identity(1), (num_combinations, 1, 1))
+    return [means, cov]
+
+
+def compute_pi_fhmm(list_pi):
+    """
+    Parameters
+    -----------
+    list_pi : List of PI's of individual learnt HMMs
+
+    Returns
+    -------
+    result : Combined Pi for the FHMM
+    """
+    result = list_pi[0]
+    for i in range(len(list_pi) - 1):
+        result = np.kron(result, list_pi[i + 1])
+    return result
+
+
+def create_combined_hmm(model):
+
+    list_pi = [model[appliance].startprob_ for appliance in model]
+    list_A = [model[appliance].transmat_ for appliance in model]
+    list_means = [model[appliance].means_.flatten().tolist()
+                  for appliance in model]
+
+    pi_combined = compute_pi_fhmm(list_pi)
+    A_combined = compute_A_fhmm(list_A)
+    [mean_combined, cov_combined] = compute_means_fhmm(list_means)
+    #model_fhmm=create_combined_hmm(len(pi_combined),pi_combined, A_combined, mean_combined, cov_combined)
+    combined_model = hmm.GaussianHMM(n_components=len(
+        pi_combined), covariance_type='full', startprob=pi_combined, transmat=A_combined)
+    combined_model.covars_ = cov_combined
+    combined_model.means_ = mean_combined
+    return combined_model
+
+
+def return_sorting_mapping(means):
+    means_copy = deepcopy(means)
+    # Sorting
+    means_copy = np.sort(means_copy, axis=0)
+    # Finding mapping
+    mapping = {}
+    for i, val in enumerate(means_copy):
+        #assert val == means[np.where(val == means)[0]]
+        mapping[i] = np.where(val == means)[0][0]
+    return mapping
+
+
+def decode_hmm(length_sequence, centroids, appliance_list, states):
+    """
+    Decodes the HMM state sequence
+    """
+    power_states_dict = {}
+    hmm_states = {}
+    hmm_power = {}
+    total_num_combinations = 1
+
+    for appliance in appliance_list:
+
+        total_num_combinations *= len(centroids[appliance])
+
+    for appliance in appliance_list:
+        hmm_states[appliance] = np.zeros(length_sequence, dtype=np.int)
+        hmm_power[appliance] = np.zeros(length_sequence)
+
+    for i in range(length_sequence):
+
+        factor = total_num_combinations
+        for appliance in appliance_list:
+
+            # assuming integer division (will cause errors in Python 3x)
+            factor = factor // len(centroids[appliance])
+
+            temp = int(states[i]) / factor
+            hmm_states[appliance][i] = temp % len(centroids[appliance])
+            hmm_power[appliance][i] = centroids[
+                appliance][hmm_states[appliance][i]]
+    return [hmm_states, hmm_power]
+
+
+class FHMM(Disaggregator):
+
+    def __init__(self):
+
+        self.model = {}
+        self.predictions = pd.DataFrame()
+
+    def train(self, building, aggregate='mains', submetered='appliances',
+              disagg_features=[Measurement('power', 'active')],
+              environmental=None):
+        """Train using 1d FHMM. Places the learnt model in `model` attribute
+        """
+
+         # Get a dataframe of appliances; Since the algorithm is 1D, we need
+        # only the first Measurement
+        train_appliances = building.utility.electric.get_dataframe_of_appliances(
+            measurement=disagg_features[0])
+
+        train_mains = building.utility.electric.get_dataframe_of_mains(
+            measurement=disagg_features[0])
+
+        # Setting frequency
+        self.freq = str(int(get_sample_period(train_mains.index))) + 's'
+
+        learnt_model = OrderedDict()
+        for appliance in train_appliances:
+            print(appliance)
+            learnt_model[appliance] = hmm.GaussianHMM(2, "full")
+
+            # Data to fit
+            X = []
+
+            # Breaking data into contiguous blocks
+            for start, end in contiguous_blocks(train_mains.index):
+                #print(start, end)
+                length = train_appliances[appliance][start:end].values.size
+                # print(length)
+                # Ignore small sequences
+                if length > 50:
+                    temp = train_appliances[appliance][
+                        start:end].values.reshape(length, 1)
+                    X.append(temp)
+            # print(X)
+            # Fit
+            learnt_model[appliance].fit(X)
+
+        # Combining to make a AFHMM
+        new_learnt_models = OrderedDict()
+        for appliance in learnt_model:
+            startprob, means, covars, transmat = sort_learnt_parameters(
+                learnt_model[appliance].startprob_, learnt_model[appliance].means_, learnt_model[appliance].covars_, learnt_model[appliance].transmat_)
+            new_learnt_models[appliance] = hmm.GaussianHMM(
+                startprob.size, "full", startprob, transmat)
+            new_learnt_models[appliance].means_ = means
+            new_learnt_models[appliance].covars_ = covars
+
+        learnt_model_combined = create_combined_hmm(new_learnt_models)
+        self.individual = new_learnt_models
+        self.model = learnt_model_combined
+
+    def disaggregate(
+        self, building, disagg_features=[Measurement('power', 'active')],
+            environmental=None):
+        """Disaggregate the test data according to the model learnt previously
+        Performs 1D FHMM disaggregation        
+        """
+        test_mains = building.utility.electric.get_dataframe_of_mains(
+            measurement=disagg_features[0])
+
+        # Array of learnt states
+        learnt_states_array = []
+
+        # Break down test_data into chunks and disaggregate separately on them
+        for start, end in contiguous_blocks(test_mains.index):
+            length = test_mains[start:end].values.size
+            temp = test_mains[start:end].values.reshape(length, 1)
+            learnt_states_array.append(self.model.predict(temp))
+
+        # Model
+        means = OrderedDict()
+        for appliance in self.individual:
+            means[appliance] = self.individual[appliance].means_
+        means_copy = deepcopy(means)
+        for appliance in means:
+            means_copy[appliance] = means[
+                appliance].astype(int).flatten().tolist()
+            means_copy[appliance].sort()
+
+        decoded_power_array = []
+        decoded_states_array = []
+        for learnt_states in learnt_states_array:
+            [decoded_states, decoded_power] = decode_hmm(
+                len(learnt_states), means_copy, means_copy.keys(), learnt_states)
+            decoded_states_array.append(decoded_states)
+            decoded_power_array.append(decoded_power)
+
+        # Combining to make a DataFrame with correct index, based on the start,
+        # end time and the frequency of the data
+        dfs_list = []
+        count = 0
+        cont_blocks = contiguous_blocks(test_mains.index)
+        for i, (start, end) in enumerate(contiguous_blocks(test_mains.index)):
+            index = pd.DatetimeIndex(start=start, end=end,
+                                     freq=self.freq)
+
+            df = pd.DataFrame(decoded_power_array[i], index=index)
+            dfs_list.append(df)
+
+        self.predictions = pd.concat(dfs_list).sort_index()
---
+++ nilmtk/metrics.py
@@ -0,0 +1,424 @@
+'''Metrics to compare disaggregation performance of various algorithms
+
+Notation
+-----------
+
+Below is the notation used to mathematically define each metric. 
+
+:math:`T` - number of time slices.
+
+:math:`t` - a time slice.
+
+:math:`N` - number of appliances.
+
+:math:`n` - an appliance.
+
+:math:`y^{(n)}_t` -  ground truth power of appliance :math:`n` in time slice :math:`t`.
+
+:math:`\\hat{y}^{(n)}_t` -  estimated power of appliance :math:`n` in time slice :math:`t`.
+
+:math:`x^{(n)}_t` - ground truth state of appliance :math:`n` in time slice :math:`t`.
+
+:math:`\\hat{x}^{(n)}_t` - estimated state of appliance :math:`n` in time slice :math:`t`.
+
+Functions
+-----------
+
+'''
+
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import f1_score
+
+
+def error_energy(predicted_power, df_appliances_ground_truth):
+    '''Compute error in assigned energy
+
+    # TODO: Give a vanilla example
+    
+    .. math::
+        error^{(n)} = 
+        \\left | \\sum_t y^{(n)}_t - \\sum_t \\hat{y}^{(n)}_t \\right |
+
+    Attributes
+    ----------
+
+    predicted_power: Pandas DataFrame of type {appliance :
+         [array of predictd power]}
+
+    df_appliances_ground_truth: Pandas DataFrame of type {appliance :
+        [array of ground truth power]}
+
+    Returns
+    -------
+    error: dict of type {appliance : error in assigned energy}
+    '''
+    error = {}
+
+    for appliance in predicted_power:
+        ground_truth_energy = np.sum(
+            df_appliances_ground_truth[appliance].values)
+        predicted_energy = np.sum(predicted_power[appliance].values)
+        error[appliance] = np.abs(predicted_energy - ground_truth_energy)
+    return error
+
+
+def fraction_energy_assigned_correctly(predicted_power, df_appliances_ground_truth):
+    '''Compute fraction of energy assigned correctly
+
+    # TODO: Give a vanilla example
+    
+    .. math::
+        fraction = 
+        \\sum_n min \\left ( 
+        \\frac{\\sum_n y}{\\sum_{n,t} y}, 
+        \\frac{\\sum_n \\hat{y}}{\\sum_{n,t} \\hat{y}} 
+        \\right )
+
+    Attributes
+    ----------
+
+    predicted_power: Pandas DataFrame of type {appliance :
+         [array of predictd power]}
+
+    df_appliances_ground_truth: Pandas DataFrame of type {appliance :
+        [array of ground truth power]}
+
+    Returns
+    -------
+    re: float representing Fraction of Energy Correctly Assigned
+    '''
+
+    fraction = np.array([])
+    total_energy_predicted = np.sum(predicted_power.values)
+
+    for appliance in predicted_power:
+
+        appliance_energy_predicted = np.sum(predicted_power[appliance].values)
+
+        appliance_energy_ground_truth = np.sum(
+            df_appliances_ground_truth[appliance].values)
+        total_energy_ground_truth = np.sum(df_appliances_ground_truth.values)
+
+        fraction = np.append(
+            fraction, np.min(
+                [appliance_energy_predicted / total_energy_predicted,
+                 appliance_energy_ground_truth /
+                 total_energy_ground_truth
+                 ]))
+
+    return np.sum(fraction)
+
+
+def mean_normalized_error_power(predicted_power, df_appliances_ground_truth):
+    '''Compute mean normalized error in assigned power
+
+    # TODO: Give a vanilla example
+        
+    .. math::
+        error^{(n)} = 
+        \\frac
+        { \\sum_t {\\left | y_t^{(n)} - \\hat{y}_t^{(n)} \\right |} }
+        { \\sum_t y_t^{(n)} }
+
+    Attributes
+    ----------
+
+    predicted_power: Pandas DataFrame of type {appliance :
+         [array of predicted power]}
+
+    df_appliances_ground_truth: Pandas DataFrame of type {appliance :
+        [array of ground truth power]}
+
+    Returns
+    -------
+    mne: dict of type {appliance : MNE error}
+    '''
+
+    mne = {}
+    numerator = {}
+    denominator = {}
+
+    for appliance in predicted_power:
+        numerator[appliance] = np.sum(np.abs(predicted_power[appliance] -
+                                             df_appliances_ground_truth[appliance].values))
+        denominator[appliance] = np.sum(
+            df_appliances_ground_truth[appliance].values)
+        mne[appliance] = numerator[appliance] * 1.0 / denominator[appliance]
+    return mne
+
+
+def rms_error_power(predicted_power, df_appliances_ground_truth):
+    '''Compute RMS error in assigned power
+    
+    # TODO: Give a vanilla example
+    
+    .. math::
+            error^{(n)} = \\sqrt{ \\frac{1}{T} \\sum_t{ \\left ( y_t - \\hat{y}_t \\right )^2 } }
+
+    Attributes
+    ----------
+
+    predicted_power: Pandas DataFrame of type {appliance :
+         [array of predicted power]}
+
+    df_appliances_ground_truth: Pandas DataFrame of type {appliance :
+        [array of ground truth power]}
+
+    Returns
+    -------
+    re: dict of type {appliance : RMS error in predicted power}
+    '''
+
+    re = {}
+
+    for appliance in predicted_power:
+        re[appliance] = np.std(predicted_power[appliance] -
+                               df_appliances_ground_truth[appliance].values)
+
+    return re
+
+
+def powers_to_states(powers):
+    '''Converts power demands into binary states
+    
+    # TODO: Give a vanilla example
+
+    Attributes
+    ----------
+
+    powers: Pandas DataFrame of type {appliance :
+         [array of power]}
+
+    Returns
+    -------
+    states: Pandas DataFrame of type {appliance :
+         [array of states]}
+    '''
+
+    on_power_threshold = 50
+
+    states = pd.DataFrame(np.zeros(power.shape))
+    states[power > on_power_threshold] = 1
+
+    return states
+
+
+def confusion_matrices(predicted_states, ground_truth_states):
+    '''Compute confusion matrix between appliance states for each appliance
+
+    # TODO: Give a vanilla example
+
+    Attributes
+    ----------
+
+    predicted_state: Pandas DataFrame of type {appliance :
+         [array of predicted states]}
+
+    ground_truth_state: Pandas DataFrame of type {appliance :
+        [array of ground truth states]}
+
+    Returns
+    -------
+    dict of type {appliance : confusion matrix}
+    '''
+
+    re = {}
+
+    for appliance in predicted_states:
+        matrix = np.zeros([np.max(ground_truth_states[appliance]) + 1,
+                           np.max(ground_truth_states[appliance]) + 1])
+        for time in predicted_states[appliance]:
+            matrix[predicted_states.values[time, appliance],
+                   ground_truth_states.values[time, appliance]] += 1
+        re[appliance] = matrix
+
+    return re
+
+
+def tp_fp_fn_tn(predicted_states, ground_truth_states):
+    '''Compute counts of True Positives, False Positives, False Negatives, True Negatives
+
+    # TODO: Give a vanilla example
+    
+    .. math::
+        TP^{(n)} = 
+        \\sum_{t}
+        and \\left ( x^{(n)}_t = on, \\hat{x}^{(n)}_t = on \\right )
+        
+        FP^{(n)} = 
+        \\sum_{t}
+        and \\left ( x^{(n)}_t = off, \\hat{x}^{(n)}_t = on \\right )
+        
+        FN^{(n)} = 
+        \\sum_{t}
+        and \\left ( x^{(n)}_t = on, \\hat{x}^{(n)}_t = off \\right )
+        
+        TN^{(n)} = 
+        \\sum_{t}
+        and \\left ( x^{(n)}_t = off, \\hat{x}^{(n)}_t = off \\right )
+
+    Attributes
+    ----------
+
+    predicted_state: Pandas DataFrame of type {appliance :
+         [array of predicted states]}
+
+    ground_truth_state: Pandas DataFrame of type {appliance :
+        [array of ground truth states]}
+
+    Returns
+    -------
+    numpy array where columns represent appliances and rows represent: [TP, FP, FN, TN]
+    '''
+
+    # assumes state 0 = off, all other states = on
+    predicted_states_on = predicted_states > 0
+    ground_truth_states_on = ground_truth_states > 0
+
+    tp = np.sum(np.logical_and(predicted_states_on.values == True,
+                ground_truth_states_on.values == True), axis=0)
+    fp = np.sum(np.logical_and(predicted_states_on.values == True,
+                ground_truth_states_on.values == False), axis=0)
+    fn = np.sum(np.logical_and(predicted_states_on.values == False,
+                ground_truth_states_on.values == True), axis=0)
+    tn = np.sum(np.logical_and(predicted_states_on.values == False,
+                ground_truth_states_on.values == False), axis=0)
+
+    return np.array([tp, fp, fn, tn]).astype(float)
+
+
+def tpr_fpr(predicted_states, ground_truth_states):
+    '''Compute True Positive Rate and False Negative Rate
+
+    # TODO: Give a vanilla example
+    
+    .. math::
+        TPR^{(n)} = \\frac{TP}{\\left ( TP + FN \\right )}
+        
+        FPR^{(n)} = \\frac{FP}{\\left ( FP + TN \\right )}
+
+    Attributes
+    ----------
+
+    predicted_state: Pandas DataFrame of type {appliance :
+         [array of predicted states]}
+
+    ground_truth_state: Pandas DataFrame of type {appliance :
+        [array of ground truth states]}
+
+    Returns
+    -------
+    numpy array where columns represent appliances and rows represent: [TPR, FPR]
+    '''
+
+    tfpn = tp_fp_fn_tn(predicted_states, ground_truth_states)
+
+    tpr = tfpn[0, :] / (tfpn[0, :] + tfpn[2, :])
+    fpr = tfpn[1, :] / (tfpn[1, :] + tfpn[3, :])
+
+    return np.array([tpr, fpr])
+
+
+def precision_recall(predicted_states, ground_truth_states):
+    '''Compute Precision and Recall
+
+    # TODO: Give a vanilla example
+    
+    .. math::
+        Precision^{(n)} = \\frac{TP}{\\left ( TP + FP \\right )}
+        
+        Recall^{(n)} = \\frac{TP}{\\left ( TP + FN \\right )}
+
+    Attributes
+    ----------
+
+    predicted_state: Pandas DataFrame of type {appliance :
+         [array of predicted states]}
+
+    ground_truth_state: Pandas DataFrame of type {appliance :
+        [array of ground truth states]}
+
+    Returns
+    -------
+    numpy array where columns represent appliances and rows represent: [Precision, Recall]
+    '''
+
+    tfpn = tp_fp_fn_tn(predicted_states, ground_truth_states)
+
+    prec = tfpn[0, :] / (tfpn[0, :] + tfpn[1, :])
+    rec = tfpn[0, :] / (tfpn[0, :] + tfpn[2, :])
+
+    return np.array([prec, rec])
+
+
+def f_score(predicted_power, ground_truth_power):
+    '''Compute F1 score
+
+    # TODO: Give a vanilla example
+    
+    .. math::
+        F_score^{(n)} = \\frac
+            {2 * Precision * Recall}
+            {Precision + Recall}
+
+    Attributes
+    ----------
+
+    predicted_state: Pandas DataFrame of type {appliance :
+         [array of predicted states]}
+
+    ground_truth_state: Pandas DataFrame of type {appliance :
+        [array of ground truth states]}
+
+    Returns
+    -------
+    numpy array where columns represent appliances and rows represent F score
+    '''
+    threshold = 30
+    predicted_states = (predicted_power > threshold).astype(int)
+    ground_truth_states = (ground_truth_power > threshold).astype(int)
+    f_score_out = {}
+    for appliance in predicted_states.columns:
+        f_score_out[appliance] = f1_score(
+            ground_truth_states[[appliance]], predicted_states[[appliance]])
+    return f_score_out
+
+    #prec_rec = precision_recall(predicted_states, ground_truth_states)
+    # return (2 * prec_rec[0, :] * prec_rec[1,:]) / (prec_rec[0,:] +
+    # prec_rec[1,:])
+    # return f1_score(ground_truth_states, predicted_states)
+
+
+def hamming_loss(predicted_state, ground_truth_state):
+    '''Compute Hamming loss
+
+    # TODO: Give a vanilla example
+    
+    .. math::
+        HammingLoss = 
+        \\frac{1}{T} \\sum_{t}
+        \\frac{1}{N} \\sum_{n}
+        xor \\left ( x^{(n)}_t, \\hat{x}^{(n)}_t \\right )
+
+    Attributes
+    ----------
+
+    predicted_state: Pandas DataFrame of type {appliance :
+         [array of predicted states]}
+
+    ground_truth_state: Pandas DataFrame of type {appliance :
+        [array of ground truth states]}
+
+    Returns
+    -------
+    float of hamming_loss
+    '''
+
+    num_appliances = np.size(ground_truth_state.values, axis=1)
+
+    xors = np.sum((predicted_state.values != ground_truth_state.values),
+                  axis=1) / num_appliances
+
+    return np.mean(xors)
