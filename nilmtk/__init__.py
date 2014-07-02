81a901f0e28167a7e2bdaae9dc48c0c19ea29f80
Jack Kelly <jack-list@xlk.org.uk>
2014-07-02 13:26:30 +0100

    remove reddstore

--- nilmtk/__init__.py
+++ nilmtk/__init__.py
@@ -3,7 +3,6 @@ from nilmtk.version import version as __version__
 from nilmtk.timeframe import TimeFrame
 from nilmtk.elecmeter import ElecMeter
 from nilmtk.datastore import DataStore, HDFDataStore
-from nilmtk.reddstore import REDDStore
 from nilmtk.metergroup import MeterGroup
 from nilmtk.appliance import Appliance
 from nilmtk.building import Building
