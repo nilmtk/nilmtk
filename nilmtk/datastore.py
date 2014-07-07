dec359bf822abea795f6c0c8c8e633156b433a89
Jack Kelly <jack-list@xlk.org.uk>
2014-07-07 14:09:08 +0100

    move Key class to bottom of file

--- nilmtk/datastore.py
+++ nilmtk/datastore.py
@@ -37,61 +37,6 @@ class DataStore(object):
         self.window = TimeFrame()
 
 
-class Key(object):
-    """A location of data or metadata within NILMTK.
-    
-    Attributes
-    ----------
-    building : int
-    meter : int
-    utility : str
-    """
-
-    def __init__(self, string=None, building=None, meter=None):
-        """
-        Parameters
-        ----------
-        string : str, optional
-            e.g. 'building1/elec/meter1'
-        building : int, optional
-        meter : int, optional
-        """
-        self.utility = None
-        if string is None:
-            self.building = building
-            self.meter = meter
-        else:
-            split = string.strip('/').split('/')
-            assert split[0].startswith('building'), "The first element must be 'building<I>', e.g. 'building1'; not '{}'.".format(split[0])
-            try:
-                self.building = int(split[0].replace("building", ""))
-            except ValueError as e:
-                raise ValueError("'building' must be followed by an integer.\n{}"
-                                 .format(e))
-            if len(split) > 1:
-                self.utility = split[1]
-            if len(split) == 3:
-                assert split[2].startswith('meter')
-                self.meter = int(split[-1].replace("meter", ""))
-            else:
-                self.meter = None
-        self._check()
-
-    def _check(self):
-        assert isinstance(self.building, int)
-        assert self.building >= 1
-        if self.meter is not None:
-            assert isinstance(self.meter, int)
-            assert self.meter >= 1
-
-    def __repr__(self):
-        self._check()
-        s = "/building{:d}".format(self.building)
-        if self.meter is not None:
-            s += "/elec/meter{:d}".format(self.meter)
-        return s
-
-
 class HDFDataStore(DataStore):
     def __init__(self, filename):
         """
@@ -368,3 +313,58 @@ def join_key(*args):
     if len(key) > 1:
         key = key[:-1] # remove last trailing slash
     return key
+
+
+class Key(object):
+    """A location of data or metadata within NILMTK.
+    
+    Attributes
+    ----------
+    building : int
+    meter : int
+    utility : str
+    """
+
+    def __init__(self, string=None, building=None, meter=None):
+        """
+        Parameters
+        ----------
+        string : str, optional
+            e.g. 'building1/elec/meter1'
+        building : int, optional
+        meter : int, optional
+        """
+        self.utility = None
+        if string is None:
+            self.building = building
+            self.meter = meter
+        else:
+            split = string.strip('/').split('/')
+            assert split[0].startswith('building'), "The first element must be 'building<I>', e.g. 'building1'; not '{}'.".format(split[0])
+            try:
+                self.building = int(split[0].replace("building", ""))
+            except ValueError as e:
+                raise ValueError("'building' must be followed by an integer.\n{}"
+                                 .format(e))
+            if len(split) > 1:
+                self.utility = split[1]
+            if len(split) == 3:
+                assert split[2].startswith('meter')
+                self.meter = int(split[-1].replace("meter", ""))
+            else:
+                self.meter = None
+        self._check()
+
+    def _check(self):
+        assert isinstance(self.building, int)
+        assert self.building >= 1
+        if self.meter is not None:
+            assert isinstance(self.meter, int)
+            assert self.meter >= 1
+
+    def __repr__(self):
+        self._check()
+        s = "/building{:d}".format(self.building)
+        if self.meter is not None:
+            s += "/elec/meter{:d}".format(self.meter)
+        return s
