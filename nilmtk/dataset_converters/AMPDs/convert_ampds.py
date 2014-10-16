cf7b83a9733977e4c8335e74745e94e0aae24bb6
Rishi Baijal <rishi12084@iiitd.ac.in>
2014-10-16 22:55:35 +0530

    Added metadata functionality

--- nilmtk/dataset_converters/AMPDs/convert_ampds.py
+++ nilmtk/dataset_converters/AMPDs/convert_ampds.py
@@ -13,7 +13,7 @@ from nilmtk.measurement import LEVEL_NAMES
 from nilmtk.utils import get_module_directory
 from nilm_metadata import convert_yaml_to_hdf5
 
-def convert(inputPath, hdfFilename, scheme=1):
+def convert(inputPath, hdfFilename, scheme=1, metadataPath='/'):
 	files=[f for f in listdir(inputPath) if isfile (join(inputPath, f)) and '.csv' in f]
 #	print(files)
 	store=HDFStore(hdfFilename)
@@ -46,14 +46,20 @@ def convert(inputPath, hdfFilename, scheme=1):
 				fp=pd.read_csv(join(inputPath, sent))
                                 key=join('water', str(sent)[:len(str(sent)) - 4] + str(i+1))
                                 store.append(key, fp)
+	
 #	print(store)
 #	print(store['/natural_gas/FRG1'])
-
+#	metadata_path=join(inputPath, 'metadata.nilmtk')
+#	print (metadataPath)
+	convert_yaml_to_hdf5(metadataPath, store)
+	store.close()
 def test():
 	inputPath='/Users/rishi/Documents/Master_folder/IIITD/5th_semester/Independent_Project/AMPds'
+#	inputPath='/Users/rishi/Documents/Master_folder/IIITD/5th_semester/Independent_Project/Forked/nilmtk/nilmtk/dataset_converters/AMPDs'
 	fileName='/Users/rishi/Documents/Master_folder/IIITD/5th_semester/Independent_Project/AMPds/electricity/store.h5'
 #	fileName='store.h5'
 	ip1=join(inputPath, 'natural_gas')
-	convert(ip1, fileName, 0)
+	metadataPath='/Users/rishi/Documents/Master_folder/IIITD/5th_semester/Independent_Project/Forked/nilmtk/nilmtk/dataset_converters/AMPDs/metadata.nilmtk'
+	convert(ip1, fileName, 0, metadataPath)
 
-#test()
+test()
