c097310489410fba9bb9f8b31aa3569dc80a2d91
Oliver Parson <op106@ecs.soton.ac.uk>
2014-11-14 11:33:44 +0000

    imports for converts should be in dataset_converters/__init__.py instead of eco/__init__.py

--- nilmtk/dataset_converters/eco/__init__.py
+++
@@ -1,5 +0,0 @@
-from .redd.convert_redd import convert_redd
-from .ukdale.convert_ukdale import convert_ukdale
-from .ampds.convert_ampds import convert_ampds
-from .combed.convert_combed import convert_combed
-from .ampds.convert_ampds import convert_ampds
