f12f5ce587a5fde01b5ca133d3865141eeed0316
Jack Kelly <jack-list@xlk.org.uk>
2014-11-17 13:54:43 +0000

    one-sec ElecMeters are marked as 'disabled' by default. #184

--- nilmtk/dataset_converters/ukdale/convert_ukdale.py
+++ nilmtk/dataset_converters/ukdale/convert_ukdale.py
@@ -36,11 +36,11 @@ def convert_ukdale(ukdale_path, hdf_filename):
     # Convert 6-second data
     _convert(ukdale_path, hdf_filename, _ukdale_measurement_mapping_func, TZ)
 
-    # Convert 1-second data
-    _convert_one_sec_data(ukdale_path, hdf_filename, ac_type_map)
-
     # Add metadata
     convert_yaml_to_hdf5(join(ukdale_path, 'metadata'), hdf_filename)
+
+    # Convert 1-second data
+    _convert_one_sec_data(ukdale_path, hdf_filename, ac_type_map)
 
     print("Done converting UK-DALE to HDF5!")
 
@@ -90,5 +90,12 @@ def _convert_one_sec_data(ukdale_path, hdf_filename, ac_type_map):
         filename = join(ukdale_path, house_path, 'mains.dat')
         df = _load_csv(filename, ONE_SEC_COLUMNS, TZ)
         store.put(str(key), df, format='table')
+        
+        # Set 'disabled' metadata attributes
+        group = store._handle.get_node('/building{:d}'.format(key.building))
+        metadata = group._f_getattr('metadata')
+        metadata['elec_meters'][key.meter]['disabled'] = True
+        group._f_setattr('metadata', metadata)
         store.flush()
+
     store.close()
