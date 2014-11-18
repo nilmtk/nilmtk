5bfe02b3e5cf3bfae478820ffa597e6d2abed1a4
beckel <beckel@inf.ethz.ch>
2014-11-18 15:57:26 +0100

    Bugfix in ECO data set converter and metadata change

--- nilmtk/dataset_converters/eco/convert_eco.py
+++ nilmtk/dataset_converters/eco/convert_eco.py
@@ -26,10 +26,10 @@ Each folder has a CSV file as per each day, with each day csv file containing
 	86400 entries.
 """
 
-sm_column_name = {1:('power', 'apparent'),
-					2:('power', 'apparent'),
-					3:('power', 'apparent'),
-					4:('power', 'apparent'),
+sm_column_name = {1:('power', 'active'),
+					2:('power', 'active'),
+					3:('power', 'active'),
+					4:('power', 'active'),
 					5:('current', ''),
 					6:('current', ''),
 					7:('current', ''),
@@ -37,88 +37,96 @@ sm_column_name = {1:('power', 'apparent'),
 					9:('voltage', ''),
 					10:('voltage', ''),
 					11:('voltage', ''),
-					12:('phase angle', 'apparent'), #What property to assign to the phase angle?
-					13:('phase angle', 'apparent'),
-					14:('phase angle', 'apparent'),
-					15:('phase angle', 'apparent'),
-					16:('phase angle', 'apparent'),
+					12:('phase_angle', ''),
+					13:('phase_angle', ''),
+					14:('phase_angle', ''),
+					15:('phase_angle', ''),
+					16:('phase_angle', ''),
 					};
 
-plugs_column_name = {1:('power', 'apparent'),	
-					};
+plugs_column_name = {1:('power', 'active'),
+                    };
 
 def convert_eco(dataset_loc, hdf_filename, timezone):
-	"""
-	Parameters:
-	-----------
-	dataset_loc: str
-		The root directory where the dataset is located.
-	hdf_filename: str
-		The location where the hdf_filename is present. 
-                The directory location has to contain the 
-		hdf5file name for the converter to work.
-	timezone: str
-		specifies the timezone of the dataset.
-	"""
+    """
+    Parameters:
+    -----------
+    dataset_loc: str
+        The root directory where the dataset is located.
+    hdf_filename: str
+        The location where the hdf_filename is present. 
+        The directory location has to contain the 
+        hdf5file name for the converter to work.
+    timezone: str
+        specifies the timezone of the dataset.
+    """
 
-	# Creating a new HDF File
-	store = pd.HDFStore(hdf_filename, 'w')
-
+    # Creating a new HDF File
+    store = pd.HDFStore(hdf_filename, 'w')
+    
     check_directory_exists(dataset_loc)
-	directory_list = [i for i in listdir(dataset_loc) if '.txt' not in i]
-	directory_list.sort()
-	print directory_list
+    directory_list = [i for i in listdir(dataset_loc) if '.txt' not in i]
+    directory_list.sort()
+    print directory_list
 
-	# Traversing every folder
-	for folder in directory_list:
+    # Traversing every folder
+    for folder in directory_list:
 
-		if folder[0] == '.' or folder[-3:] == '.h5':
-			print 'Skipping folder ', folder
-			continue
-		print 'Computing for folder',folder
+        if folder[0] == '.' or folder[-3:] == '.h5':
+            print 'Skipping ', folder
+            continue
+        print 'Computing for folder',folder
 
-		#Building number and meter_flag
-		building_no = int(folder[:2])
-		meter_flag = 'sm' if 'sm_csv' in folder else 'plugs'
+        #Building number and meter_flag
+        building_no = int(folder[:2])
+        meter_flag = 'sm' if 'sm_csv' in folder else 'plugs'
 
-		dir_list = [i for i in listdir(join(dataset_loc, folder)) if isdir(join(dataset_loc,folder,i))]
-		dir_list.sort()
-		print 'Current dir list:',dir_list
+        dir_list = [i for i in listdir(join(dataset_loc, folder)) if isdir(join(dataset_loc,folder,i))]
+        dir_list.sort()
+        print 'Current dir list:',dir_list
 
-		for fl in dir_list:
-			#Meter number to be used in key
-			meter_num = 1 if meter_flag == 'sm' else int(fl) + 1
+        for fl in dir_list:
+            #Meter number to be used in key
+            meter_num = 1 if meter_flag == 'sm' else int(fl) + 1
 
-			print 'Computing for Meter no.',meter_num
+            print 'Computing for Meter no.',meter_num
 
-			fl_dir_list = [i for i in listdir(join(dataset_loc,folder,fl)) if '.csv' in i]
-			fl_dir_list.sort()
+            # if meter_num != 4:
+            #     continue
 
-			key = Key(building=building_no, meter=meter_num)
+            fl_dir_list = [i for i in listdir(join(dataset_loc,folder,fl)) if '.csv' in i]
+            fl_dir_list.sort()
 
-			for fi in fl_dir_list:
+            # import ipdb
+            # ipdb.set_trace()
 
-				#Getting dataframe for each csv file seperately
-				df_fl = _get_df(join(dataset_loc,folder,fl),fi,meter_flag)
-				df_fl.sort_index(ascending=True,inplace=True)
-				df_fl = df_fl.tz_convert(timezone)
+            key = str(Key(building=building_no, meter=meter_num))
 
-				# If table not present in hdf5, create or else append to existing data
-				if not key in store:
-					store.put(str(key), df_fl, format='Table')
-				else:
-					store.append(str(key), df_fl, format='Table')
-				store.flush()
-				print 'Building',building_no,', Meter no.',meter_num,'=> Done for ',fi[:-4]
+            for fi in fl_dir_list:
 
-	print "Data storage completed."
-	store.close()
+                #Getting dataframe for each csv file seperately
+                df_fl = _get_df(join(dataset_loc,folder,fl),fi,meter_flag)
+                # The ECO data set is already sorted by time
+                # df_fl.sort_index(ascending=True,inplace=True)
+                df_fl = df_fl.tz_convert(timezone)
 
-	# Adding the metadata to the HDF5file
-	print "Proceeding to Metadata conversion..."
-	meta_path = join(_get_module_directory(), 'metadata')
-	convert_yaml_to_hdf5(meta_path, hdf_filename)
-	print "Completed Metadata conversion."
+                # If table not present in hdf5, create or else append to existing data
+                if not key in store:
+                    store.put(key, df_fl, format='Table')
+                    print 'Building',building_no,', Meter no.',meter_num,'=> Done for ',fi[:-4]
+                else:
+                    store.append(key, df_fl, format='Table')
+                    store.flush()
+                    print 'Building',building_no,', Meter no.',meter_num,'=> Done for ',fi[:-4]
+
+    print "Data storage completed."
+    store.close()
+
+    # Adding the metadata to the HDF5file
+    print "Proceeding to Metadata conversion..."
+    meta_path = join(_get_module_directory(), 'metadata')
+    convert_yaml_to_hdf5(meta_path, hdf_filename)
+    print "Completed Metadata conversion."
 
 def _get_module_directory():
     # Taken from http://stackoverflow.com/a/6098238/732596
@@ -134,26 +142,26 @@ def _get_module_directory():
     return path_to_this_file
 
 def _get_df(dir_loc, csv_name, meter_flag):
-	"""
-	Parameters
-	----------
-	dir_loc: str
-		Location of the directory containing the csv file.
-	csv_name: str
-		Name of the .csv file whose values are being read.
-	meter_flag: str
-		Used to differentiate between a Smart Meter and a Plug file.
-	"""
-	
-	# Changing column length for Smart Meters and Plugs
-	column_num = 16 if meter_flag == 'sm' else 1
+    """
+    Parameters
+    ----------
+    dir_loc: str
+        Location of the directory containing the csv file.
+    csv_name: str
+        Name of the .csv file whose values are being read.
+    meter_flag: str
+        Used to differentiate between a Smart Meter and a Plug file.
+    """
 
-	# Reading the CSV file and adding a datetime64 index to the Dataframe
-	df = pd.read_csv(join(dir_loc,csv_name), names=[i for i in range(1,column_num+1)])
-	df.index = pd.DatetimeIndex(start=csv_name[:-4], freq='s', periods=86400, tz = 'GMT')
+    # Changing column length for Smart Meters and Plugs
+    column_num = 16 if meter_flag == 'sm' else 1
 
-	if meter_flag == 'sm':
-		df.rename(columns=sm_column_name, inplace=True)
-	else:
-		df.rename(columns=plugs_column_name, inplace=True)
-	return df
+    # Reading the CSV file and adding a datetime64 index to the Dataframe
+    df = pd.read_csv(join(dir_loc,csv_name), names=[i for i in range(1,column_num+1)], dtype=np.float64)
+    df.index = pd.DatetimeIndex(start=csv_name[:-4], freq='s', periods=86400, tz = 'GMT')
+
+    if meter_flag == 'sm':
+        df.rename(columns=sm_column_name, inplace=True)
+    else:
+        df.rename(columns=plugs_column_name, inplace=True)
+    return df
