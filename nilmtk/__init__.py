0289f936705273404c4fe9d1b32620d3fca87b16
Jack Kelly <jack-list@xlk.org.uk>
2014-12-11 09:18:59 +0000

    Use nosetests setup and teardown to move data files from read_only to data and then deletes those temp files.  Fixes #282

--- nilmtk/__init__.py
+++ nilmtk/__init__.py
@@ -13,3 +13,33 @@ from nilmtk.building import Building
 from nilmtk.dataset import DataSet
 
 global_meter_group = MeterGroup()
+
+
+def setup_package():
+    """Nosetests package setup function (run before any tests are run).
+    See http://nose.readthedocs.org/en/latest/writing_tests.html#test-packages
+    """
+    from nilmtk.tests.testingtools import data_dir
+    from os.path import join
+    from glob import glob
+    import shutil
+
+    read_only_data = join(data_dir(), 'read_only', '*.h5')
+    for filename in glob(read_only_data):
+        print "copying", filename, "to", data_dir()
+        shutil.copy(filename, data_dir())
+
+
+def teardown_package():
+    """Nosetests package teardown function (run when tests are done).
+    See http://nose.readthedocs.org/en/latest/writing_tests.html#test-packages
+    """
+    from nilmtk.tests.testingtools import data_dir
+    from os.path import join
+    from glob import glob
+    import shutil, os
+
+    data = join(data_dir(), '*.h5')
+    for filename in glob(data):
+        print "removing", filename
+        os.remove(filename)
