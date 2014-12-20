61dfdbd0f25e3723cacef6021b2192bdf49a426d
Jack Kelly <jack-list@xlk.org.uk>
2014-12-20 20:39:22 +0000

    add 'phase angle'. #289

--- nilmtk/measurement.py
+++ nilmtk/measurement.py
@@ -9,7 +9,7 @@ AC_TYPES = ['active', 'apparent', 'reactive']
 # Note that 'pf' is deprecated. Please use 'power factor'.  See issue #289.
 PHYSICAL_QUANTITIES = ['power', 'energy', 'cumulative energy', 
                        'voltage', 'current', 'pf', 'frequency', 'power factor', 
-                       'state']
+                       'state', 'phase angle']
 PHYSICAL_QUANTITIES_WITH_AC_TYPES = ['power', 'energy', 'cumulative energy']
 PHYSICAL_QUANTITIES_TO_AVERAGE = ['voltage', 'pf', 'frequency', 'power factor']
 LEVEL_NAMES = ['physical_quantity', 'type']
