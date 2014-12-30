ae51c021df99c8c5f7ef199fc5da88da49428fa3
Nipun <nipunb@iiitd.ac.in>
2014-12-30 17:42:38 -0500

    closes #333

--- nilmtk/metergroup.py
+++ nilmtk/metergroup.py
@@ -649,7 +649,7 @@ class MeterGroup(Electric):
         columns = pd.MultiIndex.from_tuples(
             self._convert_physical_quantity_and_ac_type_to_cols(**kwargs)['cols'],
             names=LEVEL_NAMES)
-        freq = '{:d}S'.format(sample_period)
+        freq = '{:d}S'.format(int(sample_period))
         verbose = kwargs.get('verbose')
 
         # Check for empty sections
