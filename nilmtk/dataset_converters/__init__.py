2d606ff9b9a55ca0b180ed73852a67f38dfbe47b
Oliver Parson <osp@ecs.soton.ac.uk>
2015-02-05 21:37:22 +0000

    added dataport downloader import to init.py

--- nilmtk/dataset_converters/__init__.py
+++ nilmtk/dataset_converters/__init__.py
@@ -3,3 +3,4 @@ from .ukdale.convert_ukdale import convert_ukdale
 from .ampds.convert_ampds import convert_ampds
 from .combed.convert_combed import convert_combed
 from .eco.convert_eco import convert_eco
+from .dataport.download_dataport import download_dataport
