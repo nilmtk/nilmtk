192f26fc7a28b4662dc20c3a5928116d00587f9c
josemao <jmanuel.alcala@depeca.uah.es>
2015-06-09 12:58:41 +0200

    after pulling upstream

--- nilmtk/disaggregate/__init__.py
+++ nilmtk/disaggregate/__init__.py
@@ -2,5 +2,5 @@ from .disaggregator import Disaggregator
 from .combinatorial_optimisation import CombinatorialOptimisation
 from .fhmm_exact import FHMM
 from .hart_85 import Hart85
-from .maxlikelihood import MaximumLikelihood
+from .maximum_likelihood_estimation import MLE
 
