c11ff052036004184bf82e12bb7583860c1309ad
Christoph Klemenjak <christoph.klemenjak@aau.at>
2020-01-16 16:27:56 +0100

    added losses

--- nilmtk/losses.py
+++ nilmtk/losses.py
@@ -1,5 +1,9 @@
-from sklearn.metrics import mean_squared_error, mean_absolute_error, f1_score, r2_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, f1_score, r2_score, matthews_corrcoef, max_error, accuracy_score
 import numpy as np
+
+def maxerror(app_gt,app_pred):
+    # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.max_error.html#sklearn.metrics.max_error
+    return max_error(app_gt,app_pred)
 
 def mae(app_gt,app_pred):
     return mean_absolute_error(app_gt,app_pred)
@@ -15,6 +19,25 @@ def f1score(app_gt, app_pred):
     pred_temp = np.where(pred_temp<threshold,0,1)
 
     return f1_score(gt_temp, pred_temp)
+
+def accuracy(app_gt, app_pred):
+    threshold = 10
+    gt_temp = np.array(app_gt)
+    gt_temp = np.where(gt_temp<threshold,0,1)
+    pred_temp = np.array(app_pred)
+    pred_temp = np.where(pred_temp<threshold,0,1)
+
+    return accuracy_score(gt_temp, pred_temp)
+
+def matthews(app_gt,app_pred):
+    # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.matthews_corrcoef.html#sklearn.metrics.matthews_corrcoef
+    threshold = 10
+    gt_temp = np.array(app_gt)
+    gt_temp = np.where(gt_temp<threshold,0,1)
+    pred_temp = np.array(app_pred)
+    pred_temp = np.where(pred_temp<threshold,0,1)
+
+    return matthews_corrcoef(gt_temp, pred_temp)
 
 def relative_error(app_gt,app_pred):
     constant = 1
--- nilmtk/version.py
+++ nilmtk/version.py
@@ -1,2 +1,2 @@
-version = '0.4.0.dev1+git.360c76f'
+version = '0.4.0.dev1+git.bd3ee6f'
 short_version = '0.4.0'
