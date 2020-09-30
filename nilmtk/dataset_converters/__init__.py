07d9bfd364f693063a439219e5124546870b50b0
Faizan Qazi <faizanzahid09@hotmail.com>
2020-09-30 15:48:37 +0530

    added API for disaggregating site meter data only .(no submeter data required means no comparison

--- nilmtk/dataset_converters/__init__.py
+++ nilmtk/dataset_converters/__init__.py
@@ -10,3 +10,4 @@ from .hes.convert_hes import convert_hes
 from .refit.convert_refit import convert_refit
 from .iawe.convert_iawe import convert_iawe
 from .smart.convert_smart import convert_smart
+from .caxe.convert_caxe import convert_caxe
