f36191643065e49a5f60bd9e504286bf6bb00da4
Guillaume Levasseur <phenyl@posteo.net>
2021-06-07 20:58:28 +0200

    fix: API: remove hardcoded apparent power in test_jointly.
    Use the power dict instead like in the other functions

--- nilmtk/api.py
+++ nilmtk/api.py
@@ -247,7 +247,7 @@ class API():
             test=DataSet(d[dataset]['path'])
             for building in d[dataset]['buildings']:
                 test.set_window(start=d[dataset]['buildings'][building]['start_time'],end=d[dataset]['buildings'][building]['end_time'])
-                test_mains=next(test.buildings[building].elec.mains().load(physical_quantity='power', ac_type='apparent', sample_period=self.sample_period))
+                test_mains=next(test.buildings[building].elec.mains().load(physical_quantity='power', ac_type=self.power['mains'], sample_period=self.sample_period))
                 if self.DROP_ALL_NANS and self.site_only:
                     test_mains, _= self.dropna(test_mains,[])
 
