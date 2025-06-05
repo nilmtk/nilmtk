8a5dc991f929d10a3e58822990cea1a6de8ece3c
Dhru Harsh <24110107@iitgn.ac.in>
2025-06-05 19:20:38 +0530

    Add nilmtk metadata dependency and version-logic fix in setup.py

--- setup.py
+++ setup.py
@@ -1,93 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
-   Copyright 2013-2020 NILMTK developers
+Copyright 2013-2020 NILMTK developers
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
-from setuptools import setup, find_packages, Extension
-from os.path import join
 import os
 import sys
 import warnings
+import subprocess
+from setuptools import setup, find_packages
 
-# import numpy
-
+# Retrieve tag from CI if present
 TRAVIS_TAG = os.environ.get("TRAVIS_TAG", "")
 
 if TRAVIS_TAG:
-    # TODO: validate if the tag is a valid version number
     VERSION = TRAVIS_TAG
-    ISRELEASED = not ("dev" in TRAVIS_TAG)
+    ISRELEASED = "dev" not in TRAVIS_TAG
     QUALIFIER = ""
 else:
-    MAJOR = 0
-    MINOR = 4
-    MICRO = 0
-    DEV = 1  # For multiple dev pre-releases, please increment this value
+    MAJOR, MINOR, MICRO = 0, 4, 0
+    DEV = 1
     ISRELEASED = False
-    VERSION = "%d.%d.%d" % (MAJOR, MINOR, MICRO)
+    VERSION = f"{MAJOR}.{MINOR}.{MICRO}"
     QUALIFIER = ""
 
-
 FULLVERSION = VERSION
+
 if not ISRELEASED and not TRAVIS_TAG:
     try:
-        import subprocess
-
-        try:
-            pipe = subprocess.Popen(
-                ["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE
-            ).stdout
-        except OSError:
-            # msysgit compatibility
-            pipe = subprocess.Popen(
-                ["git.cmd", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE
-            ).stdout
-        rev = pipe.read().strip()
-        # makes distutils blow up on Python 2.7
-        if sys.version_info[0] >= 3:
-            rev = rev.decode("ascii")
-
-        # Use a local version tag to include the git revision
-        FULLVERSION += ".dev{}+git.{}".format(DEV, rev)
-    except:
-        FULLVERSION += ".dev{}".format(DEV)
-        warnings.warn(
-            'WARNING: Could not get the git revision, version will be "{}"'.format(
-                FULLVERSION
-            )
+        # Try standard git first
+        proc = subprocess.Popen(
+            ["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE
         )
+    except OSError:
+        # Fallback for msysgit on Windows
+        proc = subprocess.Popen(
+            ["git.cmd", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE
+        )
+    raw = proc.stdout.read().strip()
+    if sys.version_info[0] >= 3:
+        raw = raw.decode("ascii")
+    rev = raw or None
+
+    if rev:
+        FULLVERSION += f".dev{DEV}+git.{rev}"
+    else:
+        FULLVERSION += f".dev{DEV}"
 else:
     FULLVERSION += QUALIFIER
 
-
 def write_version_py(filename=None):
-    cnt = """\
+    template = """\
 version = '%s'
 short_version = '%s'
 """
     if not filename:
-        filename = os.path.join(os.path.dirname(__file__), "nilmtk", "version.py")
-
-    a = open(filename, "w", encoding="utf-8")
-    try:
-        a.write(cnt % (FULLVERSION, VERSION))
-    finally:
-        a.close()
-
+        filename = os.path.join(os.path.dirname(__file__),
+                                "nilmtk", "version.py")
+    with open(filename, "w", encoding="utf-8") as fp:
+        fp.write(template % (FULLVERSION, VERSION))
 
 write_version_py()
-# End of Version Check
 
 setup(
     name="nilmtk",
@@ -95,8 +81,9 @@ setup(
     packages=find_packages(),
     package_data={"": ["*.yaml"]},
     install_requires=[
+	"nilm_metadata @ git+https://github.com/nilmtk/nilm_metadata.git",
         "pandas>=2.1.0",
-        "numpy >= 1.26.0",
+        "numpy>=1.26.0",
         "networkx>=3.2.1",
         "scipy",
         "tables",
@@ -104,15 +91,20 @@ setup(
         "hmmlearn>=0.2.1",
         "pyyaml",
         "matplotlib>=3.10.3",
-        "jupyterlab"
+        "jupyterlab",
     ],
-    description="Estimate the energy consumed by individual appliances from "
-    "whole-house power meter readings",
+    description=(
+        "Estimate the energy consumed by individual appliances "
+        "from whole-house power meter readings"
+    ),
     author="NILMTK developers",
-    author_email="",
     url="https://github.com/nilmtk/nilmtk",
-    download_url="https://github.com/nilmtk/nilmtk/tarball/master#egg=nilmtk-dev",
+    download_url=(
+        "https://github.com/nilmtk/nilmtk/"
+        "tarball/master#egg=nilmtk-dev"
+    ),
     long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     license="Apache 2.0",
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -121,6 +113,8 @@ setup(
         "Programming Language :: Python",
         "Topic :: Scientific/Engineering :: Mathematics",
     ],
-    keywords="smartmeters power electricity energy analytics redd "
-    "disaggregation nilm nialm",
+    keywords=(
+        "smartmeters power electricity energy analytics "
+        "redd disaggregation nilm nialm"
+    ),
 )
