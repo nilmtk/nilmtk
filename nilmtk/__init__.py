2a0ef1dee38ce84bb7e28afa1debfc6d5ab2189a
Aayush Kuloor <69966888+Aayush75@users.noreply.github.com>
2025-06-08 14:13:05 +0530

    Update __init__.py to fix the pandas append error
    This fix makes sure that the API doesn't use 'append' on a pandas DataFrame. Instead, it uses 'concat' to concatenate to the DataFrame.

--- nilmtk/__init__.py
+++ nilmtk/__init__.py
@@ -1,4 +1,10 @@
 import warnings
+import pandas as pd
+
+if not hasattr(pd.DataFrame, "append"):
+    def _df_append(self, other, ignore_index=False, verify_integrity=False, sort=False):
+        return pd.concat([self, other], ignore_index=ignore_index, verify_integrity=verify_integrity, sort=sort)
+    pd.DataFrame.append = _df_append
 
 # Silence ImportWarnings for the time being
 with warnings.catch_warnings():
