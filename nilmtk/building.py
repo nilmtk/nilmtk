444800b58252ac03be8d118892c3dbc73cea4788
Mat-thias <busoyeom@gmail.com>
2025-12-22 08:50:17 +0100

    Enhance type hints in Building class; assert dataset_name in import_metadata method and made it optional, since it isnot used all the time.
    Remove the save method as it will break

--- nilmtk/building.py
+++ nilmtk/building.py
@@ -1,7 +1,8 @@
+from typing import Optional
 from collections import namedtuple, OrderedDict
 import pandas as pd
 from .metergroup import MeterGroup
-from .datastore.datastore import join_key
+from .datastore.datastore import join_key, DataStore
 from .hashable import Hashable
 
 BuildingID = namedtuple('BuildingID', ['instance', 'dataset'])
@@ -22,25 +23,27 @@ class Building(Hashable):
         self.elec = MeterGroup()
         self.metadata = {}
     
-    def import_metadata(self, store, key, dataset_name):
+    def import_metadata(self, store:DataStore, key:str, dataset_name:Optional[str]=None):
         self.metadata = store.load_metadata(key)
         if 'dataset' not in self.metadata:
+            assert dataset_name is not None, f"dataset not in metadata, please pass in the dataset_name"
             self.metadata['dataset'] = dataset_name
         elec_meters = self.metadata.pop('elec_meters', {})
         appliances = self.metadata.pop('appliances', [])
         self.elec.import_metadata(store, elec_meters, appliances, self.identifier)
                 
-    def save(self, destination, key):
-        destination.write_metadata(key, self.metadata)
-        self.elec.save(destination, join_key(key, 'elec'))
+    #  This method should be removed, elec(MeterGroup) has no group save, this would break
+    # def save(self, destination, key):
+    #     destination.write_metadata(key, self.metadata)
+    #     self.elec.save(destination, join_key(key, 'elec'))
 
     @property
-    def identifier(self):
+    def identifier(self) -> BuildingID:
         md = self.metadata
         return BuildingID(instance=md.get('instance'), 
                           dataset=md.get('dataset'))
 
-    def describe(self, **kwargs):
+    def describe(self, **kwargs) -> pd.Series:
         """Returns a Series describing this building."""
         md = self.metadata
         series = pd.Series(name=self.identifier.instance)
